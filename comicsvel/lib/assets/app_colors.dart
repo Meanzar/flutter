219cbc6cf34ded6d874f7d28a04d5aec2f7b8458
charles <charles@penguin>
2023-06-14 10:03:20 +0200

    rajout des assets de layout

---
+++ comicsvel/lib/assets/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  
+  static const fondAppli = Color(0xFFFFFBEA);
+  static const fondClair = Color(0xFFFFFDF4);
+  static const noir = Color(0xFF2C2F34);
+  static const gris = Color(0xFF414B55);
+  static const contraste = Color(0xFFCD0E1D);
+  static const contrasteFonce = Color(0xFFA3141A);
+  
+}
+
+class AppStyles {
+  static const heading1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  
+  static const bodyText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+  
+}
